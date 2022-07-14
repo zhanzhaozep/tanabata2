78c2db071a75ed18eee200fe9d7225a1f87e3f07
zhangzhaoze <220044jg@yse-c.net>
2022-07-14 09:54:11 +0900

    graph.py

--- graph.py
+++ graph.py
@@ -1,8 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-10, 10, 101)
-y = np.exp(-x**2 / 2) / np.sqrt(2 * np.pi)
+x = np.linspace(-10, 10, 101)	
 
-plt.plot(x, y)
-plt.show()
+y = np.exp(-(x-5)**2 / 2) / np.sqrt(2 * np.pi)	
+
+plt.plot(x, y)	
+plt.show()	
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-10, 10, 101)	
+y = np.exp(-x**2 / 2) / np.sqrt(2 * np.pi) + 5	
+
+plt.plot(x, y)	
+plt.show()	
